8c78d57df969eb878d4eccb7ea98602574463952
Sergey <ser.trsv@gmail.com>
2022-09-28 15:47:12 +0300

    S01E25: Code coverage and Quiz recap

---
+++ Quiz/Main/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Quiz
+//
+//  Created by Sergey Tarasov on 24.09.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+
+}
+
