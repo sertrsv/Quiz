ea3d098f614c2fa992d485241920d6c227063028
Sergey <ser.trsv@gmail.com>
2022-09-28 20:59:22 +0300

    Improve Hashable implementation

--- QuizTests/Presentation/Result/Helpers/ResultHelper.swift
+++ QuizTests/Presentation/Result/Helpers/ResultHelper.swift
@@ -7,9 +7,10 @@
 
 import QuizEngine
 
-extension Result: Hashable where Answer: Equatable {
+extension Result: Hashable where Answer: Hashable {
 	public func hash(into hasher: inout Hasher) {
-		hasher.combine(1)
+		hasher.combine(answers)
+		hasher.combine(score)
 	}
 }
 
