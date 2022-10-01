e0bf823a664f7a22165fd4276e5632091937a825
Sergey <ser.trsv@gmail.com>
2022-10-01 18:28:36 +0300

    Migrated the wrong single answer test to new APIs

--- QuizTests/Presentation/Result/ResultsPresenterTest.swift
+++ QuizTests/Presentation/Result/ResultsPresenterTest.swift
@@ -32,13 +32,10 @@ final class ResultsPresenterTest: XCTestCase {
 	}
 
 	func test_presentableAnswers_withWrongSingleAnswer_mapsAnswer() {
-		let answers = [singleAnswerQuestion: ["A1"]]
-		let correctAnswers = [singleAnswerQuestion: ["A2"]]
-		let result = Result(answers: answers, score: 0)
+		let userAnswers = [(singleAnswerQuestion, ["A1"])]
+		let correctAnswers = [(singleAnswerQuestion, ["A2"])]
 
-		let sut = ResultsPresenter(
-			result: result, questions: [singleAnswerQuestion], correctAnswers: correctAnswers
-		)
+		let sut = makeSUT(userAnswers: userAnswers, correctAnswers: correctAnswers)
 
 		XCTAssertEqual(sut.presentableAnswers.count, 1)
 		XCTAssertEqual(sut.presentableAnswers.first!.question, "Q1")
