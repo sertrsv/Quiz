36ee14b9645dd20d46df8f11b4ac32d129855f16
Sergey <ser.trsv@gmail.com>
2022-10-03 17:20:17 +0300

    Implement new question title format

--- QuizTests/Presentation/Question/QuestionPresenterTest.swift
+++ QuizTests/Presentation/Question/QuestionPresenterTest.swift
@@ -17,13 +17,13 @@ final class QuestionPresenterTest: XCTestCase {
 	func test_title_forFirstQuestion_formatsTitileForIndex() {
 		let sut = QuestionPresenter(questions: [question1, question2], question: question1)
 
-		XCTAssertEqual(sut.title, "Question #1")
+		XCTAssertEqual(sut.title, "1 of 2")
 	}
 
 	func test_title_forSecondQuestion_formatsTitileForIndex() {
 		let sut = QuestionPresenter(questions: [question1, question2], question: question2)
 
-		XCTAssertEqual(sut.title, "Question #2")
+		XCTAssertEqual(sut.title, "2 of 2")
 	}
 
 	func test_title_forUnexistentQuestion_isEmpty() {
