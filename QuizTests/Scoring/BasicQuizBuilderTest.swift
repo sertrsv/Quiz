fe9e6eb55d788fcbee2fe99b0e80a4b872e32ae5
Sergey <ser.trsv@gmail.com>
2022-10-06 02:04:16 +0300

    Enforce that every question has at least one option

--- QuizTests/Scoring/BasicQuizBuilderTest.swift
+++ QuizTests/Scoring/BasicQuizBuilderTest.swift
@@ -10,26 +10,40 @@ import QuizEngine
 
 struct BasicQuiz {
 	let questions: [Question<String>]
+	let options: [Question<String>: [String]]
+}
+
+struct NonEmptyOptions {
+	let head: String
+	let tail: [String]
 }
 
 struct BasicQuizBuilder {
 	private let questions: [Question<String>]
+	private let options: [Question<String>: [String]]
 
-	init(singleAnswerQuestion: String) {
-		self.questions = [.singleAnswer(singleAnswerQuestion)]
+	init(singleAnswerQuestion: String, options: NonEmptyOptions) {
+		let question = Question.singleAnswer(singleAnswerQuestion)
+		self.questions = [question]
+		self.options = [question: [options.head] + options.tail]
 	}
 
 	func build() -> BasicQuiz {
-		BasicQuiz(questions: questions)
+		BasicQuiz(questions: questions, options: options)
 	}
 }
 
 final class BasicQuizBuilderTest: XCTestCase {
 
     func test_initWithSingleAnswerQuestion() {
-		let sut = BasicQuizBuilder(singleAnswerQuestion: "Q1")
+		let sut = BasicQuizBuilder(
+			singleAnswerQuestion: "Q1",
+			options: NonEmptyOptions(head: "O1", tail: ["O2", "O3"])
+		)
+		let result = sut.build()
 
-		XCTAssertEqual(sut.build().questions, [.singleAnswer("Q1")])
+		XCTAssertEqual(result.questions, [.singleAnswer("Q1")])
+		XCTAssertEqual(result.options, [.singleAnswer("Q1"): ["O1", "O2", "O3"]])
     }
 
 }
