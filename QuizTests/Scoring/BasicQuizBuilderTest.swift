8c81d629f356796478dfd0cdc52c91efde72a88c
Sergey <ser.trsv@gmail.com>
2022-10-06 14:43:47 +0300

    Remove duplication

--- QuizTests/Scoring/BasicQuizBuilderTest.swift
+++ QuizTests/Scoring/BasicQuizBuilderTest.swift
@@ -24,9 +24,9 @@ struct NonEmptyOptions {
 }
 
 struct BasicQuizBuilder {
-	private var questions: [Question<String>]
-	private var options: [Question<String>: [String]]
-	private var correctAnswers: [(Question<String>, [String])]
+	private var questions: [Question<String>] = []
+	private var options: [Question<String>: [String]] = [:]
+	private var correctAnswers: [(Question<String>, [String])] = []
 
 	enum AddingError: Equatable, Error {
 		case duplicateOptions([String])
@@ -34,49 +34,22 @@ struct BasicQuizBuilder {
 		case duplicateQuestion(Question<String>)
 	}
 
-	private init(questions: [Question<String>], options: [Question<String> : [String]], correctAnswers: [(Question<String>, [String])]) {
+	private init(
+		questions: [Question<String>],
+		options: [Question<String> : [String]],
+		correctAnswers: [(Question<String>, [String])]
+	) {
 		self.questions = questions
 		self.options = options
 		self.correctAnswers = correctAnswers
 	}
 
 	init(singleAnswerQuestion: String, options: NonEmptyOptions, answer: String) throws {
-		let allOptions = options.all
-
-		guard allOptions.contains(answer) else {
-			throw AddingError.missingAnswerOptions(answer: [answer], options: allOptions)
-		}
-
-		guard Set(allOptions).count == allOptions.count else {
-			throw AddingError.duplicateOptions(allOptions)
-		}
-
-		let question = Question.singleAnswer(singleAnswerQuestion)
-		self.questions = [question]
-		self.options = [question: allOptions]
-		self.correctAnswers = [(question, [answer])]
+		try add(singleAnswerQuestion: singleAnswerQuestion, options: options, answer: answer)
 	}
 
 	mutating func add(singleAnswerQuestion: String, options: NonEmptyOptions, answer: String) throws {
-		let question = Question.singleAnswer(singleAnswerQuestion)
-
-		guard !questions.contains(question) else {
-			throw AddingError.duplicateQuestion(question)
-		}
-
-		let allOptions = options.all
-
-		guard allOptions.contains(answer) else {
-			throw AddingError.missingAnswerOptions(answer: [answer], options: allOptions)
-		}
-
-		guard Set(allOptions).count == allOptions.count else {
-			throw AddingError.duplicateOptions(allOptions)
-		}
-
-		self.questions += [question]
-		self.options[question] = allOptions
-		self.correctAnswers += [(question, [answer])]
+		self = try adding(singleAnswerQuestion: singleAnswerQuestion, options: options, answer: answer)
 	}
 
 	func adding(singleAnswerQuestion: String, options: NonEmptyOptions, answer: String) throws -> BasicQuizBuilder {
