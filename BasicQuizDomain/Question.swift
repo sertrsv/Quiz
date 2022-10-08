069754bb546b51e61ddf6d12c74bc47cad8e333a
Sergey <ser.trsv@gmail.com>
2022-10-08 10:49:00 +0300

    FIx formatting

--- BasicQuizDomain/Question.swift
+++ BasicQuizDomain/Question.swift
@@ -5,7 +5,7 @@
 //  Created by Sergey Tarasov on 26.09.2022.
 //
 
-public enum Question<T: Hashable> : Hashable {
+public enum Question<T: Hashable>: Hashable {
 	case singleAnswer(T)
 	case multipleAnswer(T)
 }
